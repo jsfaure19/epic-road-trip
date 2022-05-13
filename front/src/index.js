e73b370e3dce915f52526b56f1b17c36dfdb483d
Alban Aragon <albanaragon@MacBook-Pro-de-Alban.local>
2022-05-13 16:14:57 +0200

    feat-router

--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Trip from "./routes/Trip";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="trip" element={<Trip />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
