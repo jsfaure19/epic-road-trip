aee63ee34276ed29b802e564a0f3eceefcf4a4b4
JsFaure <jsfaure@mobilygreen.fr>
2022-06-02 11:33:16 +0200

    fix test / add workflow

--- Docker/api/app/models/user.rb
+++ Docker/api/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  # Validations
+  validates :email, :password, presence: true
+
 end
--- Docker/api/spec/controllers/users/registration_spec.rb
+++ Docker/api/spec/controllers/users/registration_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Registration', type: :request do
   describe 'Registration' do
+    context 'registration with errors' do
+      it 'save the user' do
+        post '/users.json', params: {
+          user: {email: '', password: 'azerty'}
+        }
+        expect(User.count).to eq(0)
+      end
+    end
+
     context 'registration without errors' do
       it 'save the user' do
-        post '/users', params: {
+        post '/users.json', params: {
           user: {email: 'test@gmail.com', password: 'azerty'}
         }
         expect(User.count).to eq(1)
